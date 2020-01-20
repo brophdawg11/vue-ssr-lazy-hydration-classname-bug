19c33f968f08323665e91127cb3c6956cfdbf7c2
Matt Brophy <matt@brophy.org>
2020-01-20 13:45:45 -0500

    Updates

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,32 @@
 <template>
     <div>
         <h1>Vue Lazy Hydration Classname/Attribute Bug</h1>
-        <br><br>
+        <p>
+            This repo shows a bug in not preserving classes/attributes on the root
+            element inside a <code>LazyHydrate</code> element.
+            <br><br>
+            The second button in this page should have a green background,  but
+            you will see it is incorrectly red after mount but before hydration.
+            Clicking the first button will hydrate the second button, and cause
+            the correct styles to show.
+            <br><br>
+            This is due to the following progression of classes on the inner:
+            <code>div</code>
+        </p>
+        <ol>
+            <li>SSR - <code>div.lazy-class-outer.lazy-class</code></li>
+            <li>mounted - <code>div.lazy-class-outer</code></li>
+            <li>hydrated - <code>div.lazy-class-outer.lazy-class</code></li>
+        </ol>
+        <p>
+            The library needs to proxy along classes/attributes from the existing
+            slot element upon mount when the element is not yet hydrated.
+        </p>
+
         <button @click="hydrated = true">Click to hydrate</button>
         <br><br>
-        <LazyHydrate :triggerHydration="hydrated">
-            <div id="lazy-id" class="lazy-class">
+        <LazyHydrate :triggerHydration="hydrated" class="lazy-class-outer">
+            <div class="lazy-class">
                 <button @click="onClick">
                     Click me
                 </button>
@@ -44,11 +65,15 @@ button {
     font-size: 1.25rem;
 }
 
-#lazy-id button {
+code {
+    font-family: monospace;
+}
+
+.lazy-class-outer button {
     background-color: red;
 }
 
 .lazy-class button {
-    color: blue;
+    background-color: green;
 }
 </style>
