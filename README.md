06e6f879af4f2f012acf4cbe55d5564cac74ff69
Matt Brophy <matt@brophy.org>
2020-01-20 13:01:17 -0500

    Initial comit

---
+++ README.md
@@ -0,0 +1,9 @@
+Reproduction of a vue-lazy-hydration classname/attribute bug
+
+## Install/Build
+
+* Assuming `node@8` and `npm@5+`
+* Clone this repository
+* `npm install`
+* `npm run dev` or `npm run prod`
+* Open `http://localhost:8080/`
---
+++ src/components/Home.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <h1>Vue Lazy Hydration Classname/Attribute Bug</h1>
+        <br><br>
+        <button @click="hydrated = true">Click to hydrate</button>
+        <br><br>
+        <LazyHydrate :triggerHydration="hydrated">
+            <div id="lazy-id" class="lazy-class">
+                <button @click="onClick">
+                    Click me
+                </button>
+            </div>
+        </LazyHydrate>
+    </div>
+</template>
+
+<script>
+import LazyHydrate from 'vue-lazy-hydration';
+
+export default {
+    name: 'Home',
+    components: {
+        LazyHydrate,
+    },
+    data() {
+        return {
+            hydrated: false
+        };
+    },
+    methods: {
+        onClick() {
+            window.alert('clicked');
+        }
+    }
+};
+</script>
+
+<style>
+button {
+    width: 300px;
+    padding: 10px;
+    background-color: black;
+    color: white;
+    font-size: 1.25rem;
+}
+
+#lazy-id button {
+    background-color: red;
+}
+
+.lazy-class button {
+    color: blue;
+}
+</style>
